2e23396c7bccc710ba46a6588ae6c071e4f17157
skipmaple <skipmaple@gmail.com>
2022-02-22 00:40:58 +0800

    feat: add problem 0007.

--- algorithms/0007_reverse-interger/reverse-integer.rb
+++ algorithms/0007_reverse-interger/reverse-integer.rb
@@ -9,3 +9,14 @@ def reverse(x)
   return 0 unless res > -2**31 && res < 2**31-1
   res
 end
+
+def reverse(x)
+  a=x.abs
+  res = 0
+  while a!=0 do
+    res = res*10+a%10
+    a/=10
+  end
+  return 0 unless res > -2**31 && res < 2**31-1
+  x.negative? ? -res : res
+end
